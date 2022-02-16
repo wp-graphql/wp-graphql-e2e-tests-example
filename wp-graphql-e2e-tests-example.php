bd2890398a83a25aaaaa15812602a60c09df8f9d
Jason Bahl <jasonbahl@mac.com>
2022-02-16 10:17:48 -0700

    Scaffold the plugin with the plugin file. This is all that is needed to be a valid WordPress plugin. Of course, there's no functionality, but WordPress does recognize this as a plugin that can be activated.

---
+++ wp-graphql-e2e-tests-example.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Plugin Name: WPGraphQL End-2-End Tests Example
+ * Plugin Description: An example plugin showing how to set up End to End tests using @wordpress/env and @wordpress/scripts
+ */
