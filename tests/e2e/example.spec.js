14c05cfacdcb53a6aea725cbeba3c83baa00ae1d
Jason Bahl <jasonbahl@mac.com>
2022-02-17 13:50:39 -0700

    - adds missing `await` before the visitAdminPage call

--- tests/e2e/example.spec.js
+++ tests/e2e/example.spec.js
@@ -1,7 +1,21 @@
+import { loginUser, visitAdminPage } from '@wordpress/e2e-test-utils'
+
 describe( 'example test', () => {
 
     it( 'works', () => {
         expect( true ).toBeTruthy()
     })
+
+    it ( 'verifies the plugin is active', async () => {
+
+        // login as admin
+        await loginUser();
+
+        // visit the plugins page
+        await visitAdminPage( 'plugins.php' );
+
+        // assert that our plugin is active by checking the HTML
+
+    });
 
 })
